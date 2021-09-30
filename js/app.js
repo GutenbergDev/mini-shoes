1e891e9680a3fee23571c5ff22e13619ea7c9dfa
Gutemberg Medeiros <gutenbergdevm@hotmail.com>
2021-09-30 18:34:07 -0300

    Adicionado parte two card

---
+++ js/app.js
@@ -0,0 +1,1 @@
+document.querySelector("#modal-root > div > div > div > div.gl-modal__main-content > div > div > form > ol > li.cstudio-component-form-actions.gl-form-item.gl-vspace-bpall-small.col-s-12.no-gutters.cstudio-component-form-actions___JNrBW")
